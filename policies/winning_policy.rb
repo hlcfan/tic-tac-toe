a58101c8d4d710bfd503b6bddce4de1107b0638f
Alex S <hlcfan.yan@gmail.com>
2019-08-05 13:39:40 +0800

    Split #conditions into small methods

--- policies/winning_policy.rb
+++ policies/winning_policy.rb
@@ -23,27 +23,37 @@ class WinningPolicy
 
   def conditions
     @conditions ||= begin
-                      size = @board_size - 1
-
-                      winning_rows = 0.upto(size).each_slice(@game_size).to_a
-
-                      winning_cols = Array.new { [] }
-                      0.upto(size).each do |i|
-                        winning_cols[i%@game_size] ||= []
-                        winning_cols[i%@game_size] << i
-                      end
-
-                      winning_diagonals = [
-                        0.upto(@game_size-1).map do |i|
-                          i*@game_size + i
-                        end,
-                        0.upto(@game_size-1).map do |i|
-                          i*@game_size + @game_size - i - 1
-                        end
-                      ]
-
                       winning_rows + winning_cols + winning_diagonals
                     end
+  end
+
+  def winning_rows
+    size = @board_size - 1
+    0.upto(size).each_slice(@game_size).to_a
+  end
+
+  def winning_cols
+    size = @board_size - 1
+    winning_cols = []
+    0.upto(size).each do |i|
+      winning_cols[i%@game_size] ||= []
+      winning_cols[i%@game_size] << i
+    end
+
+    winning_cols
+  end
+
+  def winning_diagonals
+    size = @board_size - 1
+
+    [
+      0.upto(@game_size-1).map do |i|
+        i*@game_size + i
+      end,
+      0.upto(@game_size-1).map do |i|
+        i*@game_size + @game_size - i - 1
+      end
+    ]
   end
 end
 
